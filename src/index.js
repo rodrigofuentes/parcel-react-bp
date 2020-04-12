612f567b8947db6928499eada3f4001a1e9b331a
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-04-12 16:55:41 -0400

    starter React app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return <div>Hello from React!</div>
+}
+
+const el = document.getElementById('app')
+ReactDOM.render(<App />, el)
--- test.js
+++
@@ -1,6 +0,0 @@
-let count = 0
-count++
-
-console.log(count)
-
-const obj = {key: 0, key1: 2}
