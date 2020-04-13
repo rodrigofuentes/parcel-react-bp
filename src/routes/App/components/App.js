11290c62ae748dba7c22a6b569fb0ba526568bd6
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-04-13 04:24:26 -0400

    added svg to build output

--- src/routes/App/components/App.js
+++ src/routes/App/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+// import smeeLogo from './smee_logo.svg'
+import logo from '../../../assets/smee_logo.svg'
 import styles from '../styles/App.module.scss'
 
 export default function App() {
   return (
     <div>
+      <img src={logo} alt="smee logo" style={{width: '300px'}} />
       <h1 className={styles.title}>Hello from React!</h1>
     </div>
   )
