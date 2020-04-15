19a404eca29e487de7cd247cb8cb66caa3f70661
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2020-04-14 21:42:06 -0400

    rearranged file structure

--- src/routes/App/components/App.js
+++ src/routes/App/components/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import smee from '../../../assets/svg/smee_logo.svg'
-import logo from '../../../assets/parcel.png'
+import smee from '../../../assets/images/smee_logo.svg'
+import logo from '../../../assets/images/parcel.png'
 import styles from '../styles/App.module.scss'
 
-export default function App({title}) {
+function App({title}) {
   return (
     <div>
       <img src={smee} alt="smee logo" style={{width: '300px'}} />
       <h1 className={styles.title}>
-        {title} and Parcel 📦
+        {title}
         <img src={logo} alt="parcel logo" />
       </h1>
     </div>
@@ -18,9 +18,11 @@ export default function App({title}) {
 }
 
 App.defaultProps = {
-  title: 'Hello from React!',
+  title: 'Hello from React!!! and Parcel 📦',
 }
 
 App.propTypes = {
   title: PropTypes.string,
 }
+
+export default App
